865c5615409b468d3005b7a4e1181869c79d5f9e
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-03-17 17:52:42 +1100

    -Trying to continuosly run a series of function so the game can run

--- Simon_BChallenge/Simon Game Challenge Starting Files/index.js
+++ Simon_BChallenge/Simon Game Challenge Starting Files/index.js
@@ -32,6 +32,37 @@ function playGame() {
     }
   }
 
+  function pressedKey2() {
+    selector = event.target.id
+    $("#" + selector).addClass("pressed") ;
+    setTimeout(function () {
+      $("#" + selector).removeClass("pressed")
+    }, 100)
+
+    switch (selector) {
+      case "0":
+        var gsound = new Audio('sounds/green.mp3');
+        gsound.play();
+        console.log(selector)
+        break;
+      case "1":
+        var rsound = new Audio('sounds/red.mp3');
+        rsound.play();
+        break;
+      case "2":
+        var ysound = new Audio('sounds/yellow.mp3');
+        ysound.play();
+        break;
+      case "3":
+        var bsound = new Audio('sounds/blue.mp3');
+        bsound.play();
+        break;
+      default:
+    }
+    // console.log(selector)
+    return selector
+  }
+
   function nextSimon(n) {
     selector = n
     setTimeout(function () {
@@ -40,11 +71,13 @@ function playGame() {
     setTimeout(function () {
       $("#" + selector).removeClass("pressed")
     }, 500)
+    console.log(selector)
 
     switch (selector) {
       case "0":
         var gsound = new Audio('sounds/green.mp3');
         gsound.play();
+        console.log(selector)
         break;
       case "1":
         var rsound = new Audio('sounds/red.mp3');
@@ -67,6 +100,33 @@ nextSimon(A[i]);
 
 };
 
+function compareSimon(A,i) {
+console.log(event)
+}
+
+function play(A,i) {
+  $(".btn").on("click", compareSimon)
+
+}
+
+function play2 (A, i) {
+  var j = 0 ;
+  var B = [];
+  var num = 0;
+  while (j <= i) {
+
+    num = $(".btn").on("click", pressedKey2)
+    B.push(num)
+
+  }
+  // while loops continues if A == B
+  if (A == B) {
+    return True
+  }
+
+
+
+}
 // function play () {
 //
 // };
@@ -79,14 +139,16 @@ var i = 0 ;
 // Check if the game is still going
 
 
-while (i < 1) {
+while (i < 2) {
 // append the array with a new value from 0 - 3
   new_num = Math.floor(Math.random() * 4)
   A.push(new_num)
-  console.log(A, i)
+  // console.log(A, i)
+  listen(A,i)
   i++ ;
+  play(A, i)
 
-  nextSimon(new_num)
+  // nextSimon(new_num)
 
 
 
@@ -95,7 +157,7 @@ while (i < 1) {
 }
 
 
-// $(".btn").on("click", pressedKey) ;
+// $(".btn").on("click", pressedKey2) ;
 
 
 } ;
