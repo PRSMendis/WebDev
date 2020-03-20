9e4cd33ead9cc5e1507f89536c5cfbac7916e25a
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-03-20 18:26:03 +1100

    - preparation for deployment on heroku

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -17,7 +17,7 @@ app.get("/", function(req,res) {
 });
 
 // tell the server that we're running on port 3000
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 }) ;
 
