6062696676c0cb8e0e24b394d5426f522b34d01c
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-03-20 15:05:12 +1100

    -finish bmicalc and weather app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -3,23 +3,23 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser")
 
 const app = express(); 
 
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/", function(req,res) {
-    
-
-    
-
-    // res.send("Server is up and running")
-
+    res.sendFile(__dirname+ "/index.html")
 });
 
-
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=3af2dd610889ed72f41025602f7e26a7&units=metric"
-    const city = "Sydney"
+app.post("/", function(req, res) {
+    
+    // console.log(req.body.cityName)
+    // const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=3af2dd610889ed72f41025602f7e26a7&units=metric"
+    // const city = "Sydney"
+    const city = req.body.cityName
     const appKey = "3af2dd610889ed72f41025602f7e26a7"
     const units = "metric"
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appKey + "&units=" + units
@@ -46,6 +46,11 @@ app.get("/", function(req,res) {
             
         })
     })
+
+})
+
+
+
 
 app.listen(3000,function(req,res) {
     console.log("Server is running on port3000")
