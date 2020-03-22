92c165cc387393472f83c77faf40bf7417bfde82
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-03-22 13:42:31 +1100

    - New Module todolist implemented -pre styled

---
+++ todolist_v1/app.js
@@ -0,0 +1,57 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// app.use("view engine", "ejs");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  // res.send("Hello");
+
+  var today = new Date() ; 
+  // var currentDay = today.getDay();
+  
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  
+  // var day = "" ;
+  var day = today.toLocaleDateString("en-US" , options);
+  
+  // if (currentDay === 6 || currentDay === 0) {
+  //   day = "Weekend" ;
+  // } else {
+  //   day = "Weekday" ; 
+  //   // res.sendfile(__dirname + "/weekday.html
+  // }
+
+  res.render("list" , {kindOfDay: day, items: items}) ;
+
+
+});
+
+app.post("/", function(req,res){
+  item = req.body.newItem
+  console.log(item);
+
+  items.push(item)  
+
+  res.redirect("/")
+
+
+
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
