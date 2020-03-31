851848d77299729afafbab9a4a3a5335a1f75211
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-03-31 16:14:08 +1100

    -adding database functionality to todolist

---
+++ FruitsProject_2/app.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb://localhost:27017/fruitsDB", { useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/People  ", { useNewUrlParser: true, useUnifiedTopology: true })
+
+const peopleSchema = new mongoose.Schema ({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", peopleSchema)
+
+const person = new Person ({
+    name: "John",
+    age: 37,
+});
+
+person.save();
