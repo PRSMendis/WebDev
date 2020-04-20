a7132625d9b42a9775539d16d4448eb944ca349a
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-04-20 14:50:56 +1000

    -Completion of WikiAPI -Built on API -Using Express features such as app.route for organisation

--- WikiApi/app.js
+++ WikiApi/app.js
@@ -25,9 +25,13 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 //TODO
 
-// app.route("/articles").get().post().delete();
 
-app.get("/articles", function(req, res){
+
+///////////////////////////////////Requests Targetting All articles/////////////////////////////////////////////////
+
+app.route("/articles")
+
+.get(function(req, res){
   Article.find(function(err, foundArticles){
     if (!err) {
     // console.log(foundArticles)
@@ -39,7 +43,7 @@ app.get("/articles", function(req, res){
   })
 })
 
-app.post("/articles", function(req,res){
+.post(function(req,res){
   // console.log(req.body.title)
   // console.log(req.body.content)
   const  newArticle = new Article ({
@@ -52,10 +56,10 @@ app.post("/articles", function(req,res){
     } else {
       res.send(err);
     }
-  });
+  })
 })
 
-app.delete("/articles", function(req, res) {
+.delete((function(req, res) {
   Article.deleteMany(function(err){
     if (!err) {
       res.send("Succesfully deleted all articles.")
@@ -63,6 +67,72 @@ app.delete("/articles", function(req, res) {
       res.send(err)
     }
   })
+}));
+
+///////////////////////////////////Requests Targetting Specific Articles/////////////////////////////////////////////////
+
+
+app.route("/articles/:articleTitle")
+
+.get(function(req,res) {
+  Article.findOne({title:req.params.articleTitle}, function(err, foundArticle){
+    // if (!err) {
+    // // console.log(foundArticles)
+    // res.send(foundArticles)
+    // } else {
+    //   res.send(err);
+    // }
+    if (foundArticle) {
+      // console.log(foundArticles)
+      res.send(foundArticle)
+      } else {
+        // res.send(err);
+        res.send("No articles matching that title were found.");
+      }
+
+})
+})
+
+// .put(function(req,res) {
+//   Article.updateMany(
+//     {title: req.params.articleTitle},
+//     {title: req.body.title, content: req.body.content},
+//     {overwrite: true},
+//     function(err) {
+//       if(!err) {
+//         res.send("Succesfully updated Article Title: " + req.body.title + ".")
+//       } else{
+//         res.send(err)
+//       }
+//     }
+//   )
+// })
+
+.patch(function(req,res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err) {
+      if(!err) {
+        res.send("Succesfully updated Article.")
+      } else{
+        res.send(err)
+      }
+    }
+  )
+})
+
+.delete(function(req,res) {
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err) {
+      if(!err) {
+        res.send("Succesfully deleted Article.")
+      } else{
+        res.send(err)
+      }
+    }
+  )
 })
 
 
