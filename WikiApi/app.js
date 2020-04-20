481731e16f0831a4842e1fe663a0968c85f40200
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-04-20 12:34:37 +1000

    -Finished first step of REST API - routing get, post, delete

--- WikiApi/app.js
+++ WikiApi/app.js
@@ -14,7 +14,59 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect("mongodb+srv://admin-rayhan:test123@cluster0-jhblh.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+  title: String,
+  content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
 //TODO
+
+// app.route("/articles").get().post().delete();
+
+app.get("/articles", function(req, res){
+  Article.find(function(err, foundArticles){
+    if (!err) {
+    // console.log(foundArticles)
+    res.send(foundArticles)
+    } else {
+      res.send(err);
+    }
+
+  })
+})
+
+app.post("/articles", function(req,res){
+  // console.log(req.body.title)
+  // console.log(req.body.content)
+  const  newArticle = new Article ({
+    title: req.body.title,
+    content: req.body.content
+  })
+  newArticle.save(function(err) {
+    if (!err) {
+      res.send("Successfully added new article.");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+app.delete("/articles", function(req, res) {
+  Article.deleteMany(function(err){
+    if (!err) {
+      res.send("Succesfully deleted all articles.")
+    } else {
+      res.send(err)
+    }
+  })
+})
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
