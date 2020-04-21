f312e5210c6e9e420c2bb39b27a07ce9f9e9bda7
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-04-21 10:57:48 +1000

    -Adding Encryption

--- Security-Authentication/Secrets/app.js
+++ Security-Authentication/Secrets/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption")
 
 const app = express();
 
@@ -13,26 +14,76 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+})
 
-const User = new.mongoose.model("User", userSchema)
+const User = new mongoose.model("User", userSchema)
 
 app.get("/", function(req,res) {
     res.render("home");
 })
 
-app.get("/login", function(req,res) {
-    res.render("login");
-})
 
-app.get("/register", function(req,res) {
-    res.render("register");
-})
+app.route("/login")
+
+.get(
+    function(req,res){
+        res.render("login");
+    }
+)
+
+.post(
+    function(req,res) {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email:username}, function(err, foundUser){
+            if (err) {
+                console.log(err)
+            } else {
+                if (foundUser) {
+                    if (foundUser.password === password) {
+                        res.render("secrets")
+                    }
+                }
+
+            }
+        })
+
+    }
+)
+
+
+
+app.route("/register")
+
+.get(
+    function(req,res){
+        res.render("register");
+    }
+)
+
+.post(
+    function(req,res) {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        })
+        newUser.save(function(err){
+            if (err) {
+                console.log(err)
+            } else {
+                res.render("secrets");
+            }
+        })
+
+    }
+)
+
 
 
 
