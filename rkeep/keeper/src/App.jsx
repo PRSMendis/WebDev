251ae17e12d0b06c5f6fd70590b19fb31db5362f
PRSMendis <38640848+PRSMendis@users.noreply.github.com>
2020-04-30 15:55:29 +1000

    -building react

--- rkeep/keeper/src/App.jsx
+++ rkeep/keeper/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Header from './Header'
+import Note from './Note'
+import Footer from './Footer'
 
 function App() {
-  return Header()
+  return (
+    <div>
+      <Header></Header>
+      <div>
+      <Note></Note>
+      </div>
+      <Footer></Footer>
+    </div>
+  )
 }
 
 export default  App
--- rkeep/keeper/src/Header.jsx
+++ rkeep/keeper/src/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+// hclasses = 
 function Header() {
-    return <h1 className = "header">henlo</h1>
-}
+    // return <h1 className = {hclasses} >Keeper</h1>
+    // return <div className = "header"><h1 className = "header h1">Keeper</h1></div>
+    return (
+        <header>
+            <h1>
+                Keeper
+            </h1>
+        </header>
 
+    )
+    }
 export default  Header
